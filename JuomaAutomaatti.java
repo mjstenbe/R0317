32748f152411b8881a28ea29bb38af7265b5dbc2
Mika Stenberg <mika.stenberg@laurea.fi>
2018-10-12 16:14:54 +0300

    Kommentti
    Hyvä

--- JuomaAutomaatti.java
+++ JuomaAutomaatti.java
@@ -4,18 +4,18 @@ public class JuomaAutomaatti {
 	//Konstruktori
 	//testi2
 	public JuomaAutomaatti () {
-		this.teet‰=50;
+		this.teet√§=50;
 		this.kahvia=50;
 		this.kaakaota=50;
 	}
 	//Tiedot
-	private int teet‰;
+	private int teet√§;
 	private int kahvia;
 	private int kaakaota;
 	
 	//Getterit
-	public int getteet‰() {
-		return this.teet‰;
+	public int getteet√§() {
+		return this.teet√§;
 	}
 	
 	public int getkahvia() {
@@ -27,8 +27,8 @@ public class JuomaAutomaatti {
 	}
 	
 	//Setterit
-	public void setteet‰(int uusiteet‰) {
-		this.teet‰=uusiteet‰;
+	public void setteet√§(int uusiteet√§) {
+		this.teet√§=uusiteet√§;
 	}
 	
 	public void setkahvia(int uusikahvia) {
@@ -41,19 +41,19 @@ public class JuomaAutomaatti {
 	
 	//Metodit jotka tekee juomat
 	public void valmistaKahvi() {
-		//Kysyt‰‰n onko kahvia enemm‰n kuin 10
+		//Kysyt√§√§n onko kahvia enemm√§n kuin 10
 		if (this.kahvia >= 10) {
 			//Jos on otetaan 10 pois ja tulostetaan valmistusviesti
 			this.kahvia=this.kahvia-10;
 			
 		}//Jos ei tulostetaan loppuminen
-		else System.out.println("Kahvi loppu");
+		else System.out.println("Kahvi loppu"); // T√§ss√§ voisi halutessaan asettaa kahvin suoraan 0:aan.
 				
 	}
 	// Toistetaan muille valmistusmetodeille sama kuin kahville
 	public void valmistaTee() {
-		if (this.teet‰ >= 10) {
-			this.teet‰=this.teet‰ - 10;
+		if (this.teet√§ >= 10) {
+			this.teet√§=this.teet√§ - 10;
 		
 	} else
 		System.out.println("Tee loppu.");
@@ -66,7 +66,7 @@ public class JuomaAutomaatti {
 		System.out.println("Kaakao loppu.");
 	}
 	public String toString() {
-		return "Odota hetki, juomasi valmistuu. Kahvia j‰ljell‰:" + kahvia + ", teet‰ j‰ljell‰ " + teet‰ + ", kaakaota j‰ljell‰ " + kaakaota;
+		return "Odota hetki, juomasi valmistuu. Kahvia j√§ljell√§:" + kahvia + ", teet√§ j√§ljell√§ " + teet√§ + ", kaakaota j√§ljell√§ " + kaakaota;
 	}
 
 	public static void main(String[] args) {
@@ -79,10 +79,10 @@ public class JuomaAutomaatti {
 		
 		do {
 			System.out.println("****Juoma-automaatti****\n1. Kahvi\n2. Tee\n3. Kaakao\n4. Lopeta.\n*******************");
-			//syˆtet‰‰n valintaan numero 1-4 v‰lill‰.
+			//sy√∂tet√§√§n valintaan numero 1-4 v√§lill√§.
 			valinta = lukija.nextInt();
 			
-			//Jos syˆtetty luku isompi kuin 0 ja pienempi kuin 4 tulostetaan k‰yttˆliittym‰ uudelleen ja pyydet‰‰n uusi numero
+			//Jos sy√∂tetty luku isompi kuin 0 ja pienempi kuin 4 tulostetaan k√§ytt√∂liittym√§ uudelleen ja pyydet√§√§n uusi numero
 			if (valinta == 1){
 				omaKone.valmistaKahvi();
 				System.out.println(omaKone);	
